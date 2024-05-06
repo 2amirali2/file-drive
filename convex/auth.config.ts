a27cb53891b4a571bd5d4c14ab38ca674aabb38d
2amirali2 <2amirali.k.h.2@gmail.com>
2024-05-06 13:37:41 +0330

    initial setup of project

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// https://good-mullet-39.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: "https://good-mullet-39.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
