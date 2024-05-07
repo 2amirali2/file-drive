c9107af45f27a5ab72e3452c278dc2cdc146383a
aamigo <aamigodeveloper@gmail.com>
2024-05-07 11:10:35 +0330

    adding the ability to delete file

---
+++ components/shared/FileCard.tsx
@@ -0,0 +1,32 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Doc } from "@/convex/_generated/dataModel"
+import { Button } from "../ui/button"
+import FileCardAction from "./FileCardAction"
+
+const FileCard = ({ file }: { file: Doc<"files"> }) => {
+  return (
+    <Card>
+      <CardHeader className="relative">
+        <CardTitle>{file.name}</CardTitle>
+        <div className="absolute top-2 right-2">
+          <FileCardAction file={file} />
+        </div>
+        {/* <CardDescription>Card Description</CardDescription> */}
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <Button>Download</Button>
+      </CardFooter>
+    </Card>
+  )
+}
+export default FileCard
---
+++ components/shared/FileCardAction.tsx
@@ -0,0 +1,76 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { MoreVertical, TrashIcon } from "lucide-react"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+import { useState } from "react"
+import { useMutation } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { Doc } from "@/convex/_generated/dataModel"
+import { useToast } from "../ui/use-toast"
+
+const FileCardAction = ({ file }: { file: Doc<"files"> }) => {
+  const { toast } = useToast()
+  const deleteFile = useMutation(api.files.deleteFile)
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false)
+  return (
+    <>
+      <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={async () => {
+                // todo : actually delete the file
+                await deleteFile({
+                  fileId: file._id
+                })
+                toast({
+                  variant: "default",
+                  title: "File Deleted",
+                  description: "Your file is now gone from system"
+                })
+              }}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <MoreVertical />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            onClick={() => setIsConfirmOpen(true)}
+            className="flex gap-1 text-red-600 items-center cursor-pointer"
+          >
+            <TrashIcon className="w-4 h-4" /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
+}
+export default FileCardAction
